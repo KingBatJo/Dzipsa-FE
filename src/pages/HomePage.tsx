84bdec5572e9fa7b9472933f08506966e0f53e71
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-02 20:58:46 +0900

    feat: 홈에 로그인 페이지 이동 링크 추가 (#3)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <p>HomePage</p>
+
+      <Link
+        to="/login"
+        className="inline-flex rounded-xl bg-black px-4 py-2 text-sm font-medium text-white"
+      >
+        로그인
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
