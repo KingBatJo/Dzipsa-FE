ca750aa6da39d56cda609ddb71f80ddb8350699d
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-03 01:25:06 +0900

    design: AppLayout 상단 그라데이션 레이어 적용 (#5)

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,13 +3,16 @@ import { Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
   return (
-    <>
+    <div className="relative min-h-dvh">
+      {/* 상단 3/4 영역(임시) 그라데이션 레이어 */}
+      <div className="absolute inset-x-0 h-3/4 bg-linear-to-b from-[#C7C7C7] to-white" />
+
       <Header />
 
-      <main className="pt-14">
+      <main className="relative pt-14">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
