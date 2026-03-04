56b5eeb9a550a9bce38f930690deff8ee2ef6130
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-04 10:51:55 +0900

    design: 가훈 섹션 스타일 수정 (#7)

--- src/pages/home/components/MottoSection.tsx
+++ src/pages/home/components/MottoSection.tsx
@@ -3,7 +3,7 @@ import { Card } from '@/components/ui/card';
 const HomeMottoSection = ({ motto }: { motto: string }) => {
   return (
     <section className="text-center">
-      <Card className="rounded-md border-none px-2 py-3">
+      <Card className="rounded-md border border-[#BDBDBD] bg-[#EEEEEE] px-2 py-4">
         <p className="text-base font-semibold">{motto}</p>
       </Card>
     </section>
