716298870ffa90cd0649ab84ca4182d4885b02af
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-05 22:49:10 +0900

    refactor: 요약 메시지 로직 분리 및 코드 구조 정리 (#11)

--- src/pages/todos/components/HouseTodosTab.tsx
+++ src/pages/todos/components/HouseTodosTab.tsx
@@ -5,6 +5,52 @@ import { PROFILE_IMAGE } from '@/mocks/mockData';
 import { User2 } from 'lucide-react';
 import UserAvatar from '@/components/common/UserAvatar';
 
+type HouseTodosSummaryProps = {
+  total: number;
+  completed: number;
+  userName: string;
+  myRemaining: number;
+  message: string;
+};
+
+// 남은 할 일에 따른 안내 메시지
+const getTodosSummaryMessage = (
+  remaining: number,
+  myRemaining: number,
+  userName: string
+) => {
+  if (remaining === 0) return '남은 할 일이 없어요';
+  if (myRemaining === 0) return `남은 할 일 중 ${userName}님의 할 일은 없어요`;
+
+  return `${userName}님은 할 일 ${myRemaining}개가 남아있어요`;
+};
+
+// 상단 요약 카드
+const HouseTodosSummary = ({
+  total,
+  completed,
+  userName,
+  myRemaining,
+  message,
+}: HouseTodosSummaryProps) => {
+  const remaining = Math.max(total - completed, 0);
+
+  return (
+    <Card className="relative mb-4 rounded-md border-none bg-[#BDBDBD] p-4">
+      <div className="text-sm font-medium">
+        <p>
+          오늘 할 일 {total}개 중 {completed}개가 완료되었어요!
+        </p>
+        <p>{message}</p>
+        <p>{getTodosSummaryMessage(remaining, myRemaining, userName)}</p>
+      </div>
+
+      {/* 임시 */}
+      <User2 className="absolute -right-3 -bottom-3 h-38 w-38 text-[#DEDEDE]" />
+    </Card>
+  );
+};
+
 const HouseTodosTab = () => {
   return (
     <div className="space-y-8">
@@ -12,6 +58,8 @@ const HouseTodosTab = () => {
         <HouseTodosSummary
           total={6}
           completed={3}
+          myRemaining={1}
+          userName="짱구"
           message="오늘 하루 정말 수고하셨어요!"
         />
 
@@ -34,32 +82,3 @@ const HouseTodosTab = () => {
 };
 
 export default HouseTodosTab;
-
-type HouseTodosSummaryProps = {
-  total: number;
-  completed: number;
-  message: string;
-};
-
-const HouseTodosSummary = ({
-  total,
-  completed,
-  message,
-}: HouseTodosSummaryProps) => {
-  const remaining = Math.max(total - completed, 0); // 남은 할 일
-
-  return (
-    <Card className="relative mb-4 rounded-md border-none bg-[#BDBDBD] p-4">
-      <div className="text-sm font-medium">
-        <p>
-          오늘 할 일 {total}개 중 {completed}개가 완료되었어요!
-        </p>
-        <p>{message}</p>
-        <p>00님은 할 일 {remaining}개가 남아있어요</p>
-      </div>
-
-      {/* 임시 */}
-      <User2 className="absolute -right-3 -bottom-3 h-38 w-38 text-[#DEDEDE]" />
-    </Card>
-  );
-};
