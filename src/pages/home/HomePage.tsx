283a43a30626ec6aab6022f9c401b415a015571e
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-06 02:30:39 +0900

    feat: 홈 화면 할 일 목록을 오늘 내 미완료 할 일이 보이도록 수정 (#11)

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,11 @@
-import { PROFILE_IMAGE, mockMembers, mockTodoList } from '@/mocks/mockData';
+import {
+  MOCK_MY_ID,
+  MOCK_TODAY,
+  PROFILE_IMAGE,
+  mockMembers,
+  mockTodoList,
+} from '@/mocks/mockData';
+import { addLocalToTodos, getTodoSections } from '@/utils/todos';
 
 import DashboardSection from '@/pages/home/components/DashboardSection';
 import ListItemCard from '@/components/common/ListItemCard';
@@ -11,13 +18,23 @@ import { formatDueAt } from '@/utils/date';
 const HomePage = () => {
   const motto = '깨끗하게 살자!';
 
+  const myId = MOCK_MY_ID;
+  const today = MOCK_TODAY;
+
+  const todos = addLocalToTodos(mockTodoList);
+  const myTodos = todos.filter((t) => t.assigneeId === myId);
+
+  const { todayTodos } = getTodoSections(myTodos, today);
+
+  const visibleTodayTodos = todayTodos.filter((t) => !t.completed);
+
   return (
     <div className="space-y-4 p-4">
       <MottoSection motto={motto} />
       <MembersSection members={mockMembers} />
       <DashboardSection />
-      <ListSection title="나의 할 일 리스트">
-        {mockTodoList.map((todo) => (
+      <ListSection title="오늘 할 일">
+        {visibleTodayTodos.map((todo) => (
           <ListItemCard
             key={todo.id}
             title={todo.title}
