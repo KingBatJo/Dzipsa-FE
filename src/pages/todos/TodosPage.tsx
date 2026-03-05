6e556d9912511330c6b6b5464224e85449acdfd9
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-05 14:49:21 +0900

    design: 탭 스타일 적용 (#11)

--- src/pages/todos/TodosPage.tsx
+++ src/pages/todos/TodosPage.tsx
@@ -2,12 +2,18 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 const TodosPage = () => {
   return (
-    <div>
+    <div className="px-4 pt-6">
       <Tabs defaultValue="my">
-        <TabsList>
-          <TabsTrigger value="my">나의 할 일</TabsTrigger>
-          <TabsTrigger value="house">우리 집 할 일</TabsTrigger>
-          <TabsTrigger value="done">완료된 할 일</TabsTrigger>
+        <TabsList className="grid h-12 w-full grid-cols-3 p-1.5">
+          <TabsTrigger value="my" className="py-2">
+            나의 할 일
+          </TabsTrigger>
+          <TabsTrigger value="house" className="py-2">
+            우리 집 할 일
+          </TabsTrigger>
+          <TabsTrigger value="done" className="py-2">
+            완료된 할 일
+          </TabsTrigger>
         </TabsList>
 
         <TabsContent value="my">my</TabsContent>
