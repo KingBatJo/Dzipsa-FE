530b27f69f4b6f30da0971d7feea99adebbde069
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-05 14:22:45 +0900

    feat: TodosPage 탭 기본 구조 구현 (#11)

--- src/pages/todos/TodosPage.tsx
+++ src/pages/todos/TodosPage.tsx
@@ -1,5 +1,21 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 const TodosPage = () => {
-  return <div>TodosPage</div>;
+  return (
+    <div>
+      <Tabs defaultValue="my">
+        <TabsList>
+          <TabsTrigger value="my">나의 할 일</TabsTrigger>
+          <TabsTrigger value="house">우리 집 할 일</TabsTrigger>
+          <TabsTrigger value="done">완료된 할 일</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="my">my</TabsContent>
+        <TabsContent value="house">house</TabsContent>
+        <TabsContent value="done">done</TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default TodosPage;
