d110271caef9f7323a971d9a357be804071004cf
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-09 02:34:58 +0900

    feat: 온보딩 선택 화면 UI 구현 (#16)
    - 디집사 캐릭터 추가

--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -1,5 +1,55 @@
+import { Button } from '@/components/ui/button';
+import dzipsaCharacter from '@/assets/dzipsa.svg';
+
 const OnboardingPage = () => {
-  return <div>OnboardingPage</div>;
+  return (
+    <div className="flex min-h-dvh flex-col items-center justify-center py-10">
+      <div className="flex flex-col items-center">
+        <div className="relative flex flex-col items-center">
+          <div className="bg-secondary rounded-xl px-5 py-4">
+            <h1 className="text-center text-lg leading-[1.3] font-semibold">
+              <span>
+                평화로운 공동생활의 시작,
+                <br />
+                디집사입니다
+              </span>
+            </h1>
+          </div>
+
+          <div className="border-t-secondary h-0 w-0 border-x-12 border-t-18 border-x-transparent" />
+        </div>
+
+        <img
+          src={dzipsaCharacter}
+          alt="디집사 캐릭터"
+          className="h-full w-full"
+        />
+      </div>
+
+      <div className="flex w-full flex-col items-center justify-center gap-4 pt-18">
+        <p className="text-base font-semibold">어떤 방법으로 시작할까요?</p>
+
+        <div className="flex w-full flex-col gap-3 px-4 text-sm leading-5 font-medium">
+          <Button type="button" className="h-12">
+            새 집 만들기
+          </Button>
+
+          <Button
+            type="button"
+            className="h-12 bg-neutral-500 hover:bg-neutral-600"
+          >
+            초대코드로 입장하기
+          </Button>
+        </div>
+      </div>
+
+      <p className="pt-14 text-center text-sm leading-[1.3] font-medium text-[#BCBCBC]">
+        초대코드가 없을 시
+        <br />
+        방만들기를 통해 방을 생성하세요
+      </p>
+    </div>
+  );
 };
 
 export default OnboardingPage;
