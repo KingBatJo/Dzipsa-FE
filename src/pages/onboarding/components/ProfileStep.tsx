9fad7b2cdcc3ff0fdcfc783a59fe2080e1bfe73a
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-09 12:18:45 +0900

    feat: 프로필 설정 화면 UI 및 입력 필드 구현 (#16)
    - profile 이미지 추가
    - EditableInput 컴포넌트 재사용

--- src/pages/onboarding/components/ProfileStep.tsx
+++ src/pages/onboarding/components/ProfileStep.tsx
@@ -1,5 +1,50 @@
+import { useRef, useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import EditableInput from './EditableInput';
+import Profile from '@/assets/profile.svg';
+import { RefreshCw } from 'lucide-react';
+
 const ProfileStep = () => {
-  return <div>ProfileStep</div>;
+  const [nickname, setNickname] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className="pt-3">
+      <section className="flex flex-col gap-2 px-2 pb-10">
+        <h1 className="text-xl font-semibold">
+          나를 표현할 프로필과
+          <br />
+          닉네임을 설정해주세요
+        </h1>
+      </section>
+
+      <section className="flex justify-center pb-[45px]">
+        <div className="relative">
+          <img src={Profile} alt="프로필 사진" />
+
+          <Button className="absolute -right-3 bottom-0 h-fit rounded-xl p-[9px] outline-2 outline-white [&>svg]:h-6 [&>svg]:w-6">
+            <RefreshCw />
+          </Button>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-1 px-10">
+        <EditableInput
+          id="nickname"
+          ref={inputRef}
+          value={nickname}
+          placeholder="닉네임을 입력해주세요"
+          maxLength={20}
+          onChange={setNickname}
+        />
+
+        <div className="flex justify-end text-xs font-semibold text-[#BCBCBC]">
+          최대 20글자
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ProfileStep;
