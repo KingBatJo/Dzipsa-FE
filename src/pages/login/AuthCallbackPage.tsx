0b8a9fc0065d07becf4858fb346d9523dd49127a
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-12 04:11:04 +0900

    fix: 콜백 페이지 return 누락으로 인한 약관 동의 오작동 수정 (#19)

--- src/pages/login/AuthCallbackPage.tsx
+++ src/pages/login/AuthCallbackPage.tsx
@@ -9,6 +9,7 @@ const AuthCallbackPage = () => {
 
   useEffect(() => {
     const accessToken = searchParams.get('accessToken');
+    console.log('callback accessToken:', accessToken);
 
     // 토큰이 없으면 로그인 페이지로 이동
     if (!accessToken) {
@@ -18,6 +19,7 @@ const AuthCallbackPage = () => {
           loginError: '로그인 처리 중 문제가 발생했어요. 다시 시도해주세요.',
         },
       });
+      return;
     }
 
     // 메모리에 accessToken 저장
