bd2412917cec2aa23100f0b96d0c8d0889844cfa
윤진 <151013932+maruru301@users.noreply.github.com>
2026-03-26 01:21:41 +0900

    design: 회원가입 완료 화면 최종 디자인 반영 (#52)

--- src/pages/login/SignupCompletePage.tsx
+++ src/pages/login/SignupCompletePage.tsx
@@ -1,4 +1,5 @@
-import { Button } from '@/components/ui/button';
+import AppButton from '@/components/common/AppButton';
+import celebration from '@/assets/image/login/celebration.png';
 import { useNavigate } from 'react-router-dom';
 
 const SignupCompletePage = () => {
@@ -6,25 +7,31 @@ const SignupCompletePage = () => {
 
   return (
     <div className="flex min-h-dvh flex-col">
-      <div className="flex flex-1 flex-col items-center justify-center">
-        <h1 className="pt-15 text-center text-xl font-semibold">
-          환영합니다!
-          <br />
-          가입이 완료되었어요!
-        </h1>
+      <div className="flex flex-1 flex-col items-center justify-center gap-[21px] pt-[15%]">
+        <img src={celebration} alt="" className="h-[244px] w-[160px]" />
 
-        <p className="pt-2 text-center text-sm font-semibold text-[#BCBCBC]">
-          디집사와 함께 눈치 보지 않는 편안한
-          <br /> 공동생활을 시작해 보세요.
-        </p>
+        <div className="flex flex-col gap-[7px]">
+          <h1 className="text-center text-xl font-semibold">
+            환영합니다!
+            <br />
+            가입이 완료되었어요!
+          </h1>
+
+          <p className="text-center text-sm font-semibold text-zinc-400">
+            디집사와 함께 눈치 보지 않는 편안한
+            <br /> 공동생활을 시작해 보세요.
+          </p>
+        </div>
       </div>
 
-      <Button
-        onClick={() => navigate('/onboarding', { replace: true })}
-        className="mx-4 mb-18 h-12"
-      >
-        시작하기
-      </Button>
+      <div className="px-[15px] pt-[10px] pb-[50px]">
+        <AppButton
+          onClick={() => navigate('/onboarding', { replace: true })}
+          className="bg-zinc-800 text-zinc-100 hover:bg-zinc-900"
+        >
+          시작하기
+        </AppButton>
+      </div>
     </div>
   );
 };
